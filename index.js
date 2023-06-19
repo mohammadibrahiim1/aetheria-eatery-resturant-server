e0be262405eb79c9aedef2c5987a47b980c957b4
mohammadibrahiim1 <mohammadibrahiim1@gmail.com>
2023-06-19 17:20:35 +0600

    updated

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ async function run() {
 
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+     client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
